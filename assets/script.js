4f36011836c0e0b9431f859b307f3d54ef9ccab0
fatina malzir <fatina.malzir@gmail.com>
2023-12-04 23:23:01 +0100

    carrousel

--- assets/script.js
+++ assets/script.js
@@ -17,4 +17,41 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+/******* les fléches *********/
 
+const precedent = document.getElementById("precedent");
+
+precedent.addEventListener("click", function() {
+    console.log("precedent");
+});
+
+const suivant = document.getElementById("suivant");
+suivant.addEventListener("click", function() {
+
+    console.log("suivant");
+});
+
+/*******defiler des images */
+
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero > slides.length -1){
+        numero = 0;}
+
+    if (numero < 0){
+		numero = slides.length -1;}
+	
+		/****recup image */
+
+		let bannerImage = document.querySelector(".banner-img ");
+		bannerImage.src = "./assets/images/slideshow/"+slides[numero].image;
+		
+
+		/*******recup text */
+		let bannerText = document.querySelector("#banner p");
+		bannerText.innerHTML = slides[numero].tagLine;
+}
+
+setInterval("ChangeSlide(1)", 4000);
