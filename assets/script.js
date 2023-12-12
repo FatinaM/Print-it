87676aa7a9f8e77fcf75785456a4e5b592e518e4
fatina malzir <fatina.malzir@gmail.com>
2023-12-12 10:52:03 +0100

    dots

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,9 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+	
+
 /******* les fléches *********/
 
 const precedent = document.getElementById("precedent");
@@ -31,6 +34,22 @@ suivant.addEventListener("click", function() {
     console.log("suivant");
 });
 
+//Ajout des bullet ponts en fonction du nombre de slides
+
+let conteneur = document.querySelector(".dots");
+		
+for (let i = 0; i < slides.length; i++) {
+
+	let nouvelleDiv = document.createElement("div");
+
+	nouvelleDiv.classList.add("dot");
+
+	conteneur.appendChild(nouvelleDiv);
+}
+
+// Marquer manuellement le premier point comme sélectionné
+document.querySelector('.dot:first-child').classList.add('dot_selected');
+
 /*******defiler des images */
 
 let numero = 0;
@@ -43,15 +62,25 @@ function ChangeSlide(sens) {
     if (numero < 0){
 		numero = slides.length -1;}
 	
-		/****recup image */
-
-		let bannerImage = document.querySelector(".banner-img ");
+		/****recup image */		
+	let bannerImage = document.querySelector(".banner-img ");
 		bannerImage.src = "./assets/images/slideshow/"+slides[numero].image;
 		
-
 		/*******recup text */
-		let bannerText = document.querySelector("#banner p");
+	let bannerText = document.querySelector("#banner p");
 		bannerText.innerHTML = slides[numero].tagLine;
-}
+		
+	
 
-setInterval("ChangeSlide(1)", 4000);
+		//Selectionne le dot en fonction du slide actif
+		let dots = document.querySelectorAll('.dot');
+		dots.forEach((dot, index) => {
+			if (index === numero) {
+		        dot.classList.add('dot_selected'); // Ajouter la classe .dot_selected au bullet point correspondant au slide actuel
+		    } else {
+		        dot.classList.remove('dot_selected'); // Supprimer la classe .dot_selected des autres bullet points
+		    }
+		});
+	}
+
+//setInterval("ChangeSlide(1)", 4000);//
